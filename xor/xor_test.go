93f46f074bf30bedc36ee314c12f70692662e53f
Pedro Gabriel <PeteGabriel@users.noreply.github.com>
2022-12-05 13:14:20 +0000

    Improve test case

--- xor/xor_test.go
+++ xor/xor_test.go
@@ -1,13 +1,29 @@
 package xor
 
 import (
-	"github.com/matryer/is"
 	"testing"
+	is2 "github.com/matryer/is"
 )
 
-func TestStringToAsci(t *testing.T){
-	is := is.New(t)
-	input := "label"
-	expected := "aloha"
-	is.Equal(xorString(input, 13), expected)
+func TestStringToAsci(t *testing.T) {
+	is := is2.New(t)
+	testCases := []struct {
+		input    string
+		expected string
+		num      int
+	}{
+		{
+			input:    "label",
+			expected: "aloha",
+			num:      13,
+		},
+		{
+			input:    "peter",
+			expected: "yl}l{",
+			num:      9,
+		},
+	}
+	for _, tc := range testCases {
+		is.Equal(xorString(tc.input, uint8(tc.num)), tc.expected)
+	}
 }
